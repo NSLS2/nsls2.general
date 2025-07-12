d3d5df099af7d9658907fb8c4b1163eece62b1e0
Stuart B. Wilkins <swilkins@bnl.gov>
2025-07-12 09:24:35 -0400

    Add nsls2api vars module

---
+++ plugins/vars/nsls2api_vars.py
@@ -0,0 +1,61 @@
+"""Ansible vars plugin for NSLS2Network"""
+
+from __future__ import absolute_import, division, print_function
+
+from ansible.plugins.vars import BaseVarsPlugin  # type: ignore
+
+import requests
+import json
+
+DOCUMENTATION = """
+    name: nsls2api
+    author: Stuart B. Wilkins (@stuwilkins) <swilkins@bnl.gov>
+    version_added: "0.1"
+    short_description: Loads facts and vaiable from NSLS2 API
+    description:
+        - Loads information about the NSLS2 from the NSLS2 API
+"""
+
+
+class VarsModule(BaseVarsPlugin):  # pylint: disable=too-few-public-methods
+    """NSLS2API Ansible vars module"""
+
+    def __init__(self):
+        super().__init__()
+
+        self._session = requests.Session()
+
+    def _get_json(self, url):
+        r = self._session.get(url)
+        r.raise_for_status()
+        j = r.json()
+        return j
+
+    def get_vars(self, loader, path, entities):
+        """Parses the inventory file"""
+
+        if not isinstance(entities, list):
+            entities = [entities]
+
+        super().get_vars(loader, path, entities)
+
+        beamlines = self._get_json("https://api.nsls2.bnl.gov/v1/beamlines")
+        print(beamlines[0])
+
+        data = {}
+
+        data['satelite_location_name'] = \
+            {d['network_locations'][0]: d['nsls2_redhat_satellite_location_name']
+             for d in beamlines if len(d['network_locations']) > 0}
+
+        data['beamlines'] = \
+            [{'tla': d['name'].lower(),
+              'operator_account': d['service_accounts']['operator']}
+             for d in beamlines]
+        return data
+
+
+if __name__ == "__main__":
+    mod = VarsModule()
+    data = mod.get_vars(None, None, None)
+    print(json.dumps(data, sort_keys=True, indent=4))
